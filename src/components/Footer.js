e9d418312bdf6bd57f4c43e1cdf08c8c4c0bf714
DheenaBharath20 <bharathkuppu@gmail.com>
2021-09-27 08:12:38 +0530

    Footer JS

---
+++ src/components/Footer.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="page-footer font-small indigo">
+        <div className="container text-center text-md-left">
+        <div className="row">
+        <div className="col-md-3 mx-auto">
+          <h5 className="font-weight-bold text-uppercase mt-3 mb-4 active">Links</h5>  
+          <ul className="list-unstyled">
+            <li>
+              <a href="#!">Link 1</a>
+            </li>
+            <li>
+              <a href="#!">Link 2</a>
+            </li>
+            <li>
+              <a href="#!">Link 3</a>
+            </li>
+            <li>
+              <a href="#!">Link 4</a>
+            </li>
+          </ul>
+  
+        </div>
+        <hr className="clearfix w-100 d-md-none"/>
+        <div className="col-md-3 mx-auto">
+          <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+  
+          <ul className="list-unstyled">
+            <li>
+              <a href="#!">Link 1</a>
+            </li>
+            <li>
+              <a href="#!">Link 2</a>
+            </li>
+            <li>
+              <a href="#!">Link 3</a>
+            </li>
+            <li>
+              <a href="#!">Link 4</a>
+            </li>
+          </ul>
+  
+        </div>
+        <hr className="clearfix w-100 d-md-none"/>
+        <div className="col-md-3 mx-auto">
+          <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+  
+          <ul className="list-unstyled">
+            <li>
+              <a href="#!">Link 1</a>
+            </li>
+            <li>
+              <a href="#!">Link 2</a>
+            </li>
+            <li>
+              <a href="#!">Link 3</a>
+            </li>
+            <li>
+              <a href="#!">Link 4</a>
+            </li>
+          </ul>
+  
+        </div>
+        <hr className="clearfix w-100 d-md-none"/>
+          <div className="col-md-3 mx-auto">
+          <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+          <ul className="list-unstyled">
+            <li>
+              <a href="#!">Link 1</a>
+            </li>
+            <li>
+              <a href="#!">Link 2</a>
+            </li>
+            <li>
+              <a href="#!">Link 3</a>
+            </li>
+            <li>
+              <a href="#!">Link 4</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      </div>
+      </footer>
+        )
+}
+
+export default Footer
